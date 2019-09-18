9fa0ba729cc49eeeee565b28ac088a685a8622e9
safoex <safoex@hotmail.com>
2019-09-18 19:09:09 +0300

    changed file for paper

---
+++ src/io/ros/ROSSimpleActionClient.h
@@ -0,0 +1,14 @@
+//
+// Created by safoex on 02.09.19.
+//
+
+#ifndef ABTM_SIMPLEACTIONCLIENT_H
+#define ABTM_SIMPLEACTIONCLIENT_H
+
+
+class SimpleActionClient {
+
+};
+
+
+#endif //ABTM_SIMPLEACTIONCLIENT_H
